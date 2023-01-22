38ed381ab7b26feed83f34c7abf2f2ab85b08fbb
Saad Ali <saad@nixknight.net>
2023-01-22 17:28:25 +0500

    Update to v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World! Canary Deployment v1"
+    return "Hello, World! Canary Deployment v2"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
